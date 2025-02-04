1eb7847fe244e7988c656a62e9e2ed8dab156b39
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-04 14:34:49 +0900

    入力要件の詳細を追加

---
+++ Basic/app/oneDimensionArray/numbers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+int main(){
+  std::vector<int> numbers;
+  while (true)
+  {
+    std::cout << "-------------------------------------------\n" 
+    << "a: " << "値を追加します。" << "\n" 
+    << "-------------------------------------------\n";
+  }
+  
+  return 0;
+}
