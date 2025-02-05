870835961528d9a5799837be5f7b7c4359118310
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-05 17:12:49 +0900

    関数名を変更

--- Basic/app/oneDimensionArray/numbers/test.cpp
+++ Basic/app/oneDimensionArray/numbers/test.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <cassert>
 #include "numbers.hpp"
-void testAdd()
+void TestAdd()
 {
   std::cout << "テスト開始" << std::endl;
+  
   std::vector<int> numbers1;
   app::add(numbers1, std::to_string(0));
   app::add(numbers1, std::to_string(1));
   app::add(numbers1, std::to_string(2));
   app::add(numbers1, std::to_string(3));
   app::add(numbers1, std::to_string(4));
+  
   std::vector<int> numbers2{0, 1, 2, 3, 4};
   auto index = 0;
   for (const auto &v : numbers1)
@@ -22,16 +24,19 @@ void testAdd()
   std::cout << "テスト成功" << std::endl;
 }
 
-void testRemove()
+void TestRemove()
 {
   std::cout << "テスト開始" << std::endl;
+  
   std::vector<int> numbers1;
   app::add(numbers1, std::to_string(0));
   app::add(numbers1, std::to_string(1));
   app::add(numbers1, std::to_string(2));
   app::add(numbers1, std::to_string(3));
   app::add(numbers1, std::to_string(4));
+  
   app::remove(numbers1, std::to_string(0));
+
   std::vector<int> numbers2{1, 2, 3, 4};
   auto index = 0;
   for (const auto &v : numbers1)
@@ -43,17 +48,20 @@ void testRemove()
   std::cout << "テスト成功" << std::endl;
 }
 
-void testUpdate()
+void TestUpdate()
 {
   std::cout << "テスト開始" << std::endl;
+  
   std::vector<int> numbers1;
   app::add(numbers1, std::to_string(0));
   app::add(numbers1, std::to_string(1));
   app::add(numbers1, std::to_string(2));
   app::add(numbers1, std::to_string(3));
   app::add(numbers1, std::to_string(4));
+  
   app::update(numbers1, std::to_string(0), std::to_string(10));
   app::update(numbers1, std::to_string(1), std::to_string(11));
+  
   std::vector<int> numbers2{10, 11, 2, 3, 4};
   auto index = 0;
   for (const auto &v : numbers1)
@@ -68,24 +76,28 @@ void testUpdate()
 void TestFind()
 {
   std::cout << "テスト開始" << std::endl;
+  
   std::vector<int> numbers1;
   app::add(numbers1, std::to_string(0));
   app::add(numbers1, std::to_string(1));
   app::add(numbers1, std::to_string(2));
   app::add(numbers1, std::to_string(3));
   app::add(numbers1, std::to_string(4));
+  
   auto flag1 = app::find(numbers1, std::to_string(2));
   assert(flag1 == true);
+  
   auto flag2 = app::find(numbers1, std::to_string(12));
   assert(flag2 == false);
+  
   std::cout << "テスト成功" << std::endl;
 }
 
 int main()
 {
-  // testAdd();
-  // testRemove();
-  // testUpdate();
+  TestAdd();
+  TestRemove();
+  TestUpdate();
   TestFind();
   return 0;
 }
