384ce455b4a9803d8de1d9c476bd8e084d59f326
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-05 17:09:42 +0900

    戻り値を論理型に変更 + Find関数の実装

--- Basic/app/oneDimensionArray/numbers/numbers.cpp
+++ Basic/app/oneDimensionArray/numbers/numbers.cpp
@@ -12,64 +12,85 @@ bool app::isNumber(std::string &s)
   return false;
 }
 
-void app::add(std::vector<int> &numbers, std::string number)
+bool app::add(std::vector<int> &numbers, std::string number)
 {
   if (isNumber(number) == false)
   {
     std::cout << "入力値が正しくありません。数値を追加できませんでした。\n";
+    return false;
   }
-  else
-  {
-    std::cout << "新しい数値を追加します。\n";
-    numbers.push_back(std::stoi(number));
-  }
+
+  std::cout << "新しい数値を追加します。\n";
+  numbers.push_back(std::stoi(number));
+  return true;
 }
 
-void app::remove(std::vector<int> &numbers, std::string number)
+bool app::remove(std::vector<int> &numbers, std::string number)
 {
   if (isNumber(number) == false)
   {
     std::cout << "入力値が正しくありません。数値を削除できませんでした。\n";
+    return false;
   }
-  else
+  auto iterator = std::find_if(std::cbegin(numbers), std::cend(numbers), [number](int value)
+                               { return value == std::stoi(number); });
+  if (iterator == std::cend(numbers))
   {
-    auto iterator = std::find_if(std::cbegin(numbers), std::cend(numbers), [number](int value)
-                                 { return value == std::stoi(number); });
-    if (iterator == std::cend(numbers))
-    {
-      std::cout << "指定した数値は存在しませんでした。\n";
-    }
-    else
-    {
-      numbers.erase(iterator);
-      std::cout << "指定した数値は削除しました。\n";
-    }
+    std::cout << "指定した数値は存在しませんでした。\n";
+    return false;
   }
+  numbers.erase(iterator);
+  std::cout << "指定した数値は削除しました。\n";
+  return true;
 }
 
-void app::update(std::vector<int> &numbers, std::string number1, std::string number2)
+bool app::update(std::vector<int> &numbers, std::string number1, std::string number2)
 {
   std::cout << "数値を更新します\n";
   if (isNumber(number1) == false && isNumber(number2) == false)
   {
     std::cout << "入力値が正しくありません。数値を更新できませんでした。\n";
+    return false;
   }
-  else
-  {
-    for (auto &v : numbers)
-    {
-      if (v == std::stoi(number1))
-      {
-        v = std::stoi(number2);
-      }
-    }
 
-    std::cout << "新しい値に更新しました。\n";
+  for (auto &v : numbers)
+  {
+    if (v == std::stoi(number1))
+    {
+      v = std::stoi(number2);
+    }
   }
+
+  std::cout << "新しい値に更新しました。\n";
+  return true;
 }
 
-void app::find(std::vector<int> &numbers, std::string number)
+bool app::find(std::vector<int> &numbers, std::string number)
 {
+  std::cout << "数値を検索します。\n";
+  if (isNumber(number) == false)
+  {
+    std::cout << "入力値が正しくありません。数値を検索できませんでした。\n";
+    return false;
+  }
+
+  auto iterator = std::find_if(std::cbegin(numbers), std::cend(numbers), [number](int value)
+                               { return value == std::stoi(number); });
+  if (iterator == std::cend(numbers))
+  {
+    std::cout << "指定した数値は存在しませんでした。\n";
+    return false;
+  }
+
+  for (const auto &v : numbers)
+  {
+    if (v == std::stoi(number))
+    {
+      std::cout << v << "\n";
+    }
+  }
+
+  return true;
 }
 
 void app::sortAsc(std::vector<int> &numbers)
--- Basic/app/oneDimensionArray/numbers/numbers.hpp
+++ Basic/app/oneDimensionArray/numbers/numbers.hpp
@@ -13,23 +13,27 @@ namespace app
   /// @brief 値を数値リストに追加する。
   /// @param numbers 数値リスト
   /// @param number 追加する数値
-  void add(std::vector<int> &numbers, std::string number);
+  /// @return 正常に数値を追加できた場合true、追加できなかった場合falseを返します。
+  bool add(std::vector<int> &numbers, std::string number);
 
   /// @brief 値を数値リストから削除する。
   /// @param numbers 数値リスト
   /// @param number 削除する数値
-  void remove(std::vector<int> &numbers, std::string number);
+  /// @return 正常に数値を削除できた場合true、削除できなかった場合falseを返します。
+  bool remove(std::vector<int> &numbers, std::string number);
 
   /// @brief 数値リストの値を更新する。
   /// @param numbers 数値リスト
   /// @param number1 更新前の数値
   /// @param number2 更新後の数値
-  void update(std::vector<int> &numbers, std::string number1, std::string number2);
+  /// @return 正常に数値を更新できた場合true、更新できなかった場合falseを返します。
+  bool update(std::vector<int> &numbers, std::string number1, std::string number2);
 
   /// @brief 目的の値を数値リストから探索する。
   /// @param numbers 数値リスト
   /// @param number 探索する数値
-  void find(std::vector<int> &numbers, std::string number);
+  /// @return 正常に数値を検索できた場合true、検索できなかった場合falseを返します。
+  bool find(std::vector<int> &numbers, std::string number);
 
   /// @brief 数値リストを昇順にソートする。
   /// @param numbers 数値リスト
