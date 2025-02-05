e4316728ba34e4155b04d21e2fd13e1e07463d06
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-05 11:19:47 +0900

    更新関数テストを実装

--- Basic/app/oneDimensionArray/numbers/test.cpp
+++ Basic/app/oneDimensionArray/numbers/test.cpp
@@ -43,9 +43,32 @@ void testRemove()
   std::cout << "テスト成功" << std::endl;
 }
 
+void testUpdate()
+{
+  std::cout << "テスト開始" << std::endl;
+  std::vector<int> numbers1;
+  app::add(numbers1, std::to_string(0));
+  app::add(numbers1, std::to_string(1));
+  app::add(numbers1, std::to_string(2));
+  app::add(numbers1, std::to_string(3));
+  app::add(numbers1, std::to_string(4));
+  app::update(numbers1, std::to_string(0), std::to_string(10));
+  app::update(numbers1, std::to_string(1), std::to_string(11));
+  std::vector<int> numbers2{10, 11, 2, 3, 4};
+  auto index = 0;
+  for (const auto &v : numbers1)
+  {
+    assert(v == numbers2.at(index));
+    index += 1;
+  }
+
+  std::cout << "テスト成功" << std::endl;
+}
+
 int main()
 {
   // testAdd();
-  testRemove();
+  // testRemove();
+  testUpdate();
   return 0;
 }
