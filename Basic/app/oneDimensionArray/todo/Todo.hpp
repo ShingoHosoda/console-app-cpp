d1895f083ccd67963feec887bdbb88c54b2541dc
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-07 10:52:48 +0900

    比較演算子のオーバーロードと出力ストリームのオーバーロードを追加

--- Basic/app/oneDimensionArray/todo/Todo.hpp
+++ Basic/app/oneDimensionArray/todo/Todo.hpp
@@ -23,6 +23,18 @@ namespace app
     /// @param _dueDate TODOの期限日
     /// @param _completed TODOが完了したかどうか
     Todo(std::string _id, std::string _title, std::string _dueDate, bool _completed) noexcept : id{_id}, title{_title}, dueDate{_dueDate}, completed{_completed} {}
+
+    [[nodiscard]]
+    friend constexpr bool operator==(Todo &todo1, Todo &todo2)
+    {
+      return (todo1.id) == (todo2.id) && (todo1.title) == (todo2.title) && (todo1.dueDate) == (todo2.dueDate) && (todo1.completed) == (todo2.completed);
+    }
+
+    template <class CharType>
+    friend std::basic_ostream<CharType> &operator<<(std::basic_ostream<CharType> &output, const Todo &todo)
+    {
+      return output << todo.id << CharType(',') << CharType(' ') << todo.title << CharType(',') << CharType(' ') << todo.dueDate << CharType(',') << CharType(' ') << todo.completed;
+    }
   };
 
 }
