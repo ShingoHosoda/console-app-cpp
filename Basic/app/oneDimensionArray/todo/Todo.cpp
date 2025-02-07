802e55d6a4b303e2faf5f81d9ed3fac88317aaba
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-07 12:00:11 +0900

    TODOアプリのソート以外の関数定義

---
+++ Basic/app/oneDimensionArray/todo/Todo.cpp
@@ -0,0 +1,47 @@
+#include "Todo.hpp"
+
+bool app::add(std::vector<Todo> &todos)
+{
+  return false;
+}
+
+bool app::remove(std::vector<Todo> &todos)
+{
+  return false;
+}
+
+bool app::update(std::vector<Todo> &todos)
+{
+  return false;
+}
+
+void app::findByTitle(const std::vector<Todo> &todos)
+{
+}
+
+void app::findByDueDate(const std::vector<Todo> &todos)
+{
+}
+
+void app::findByCompleted(const std::vector<Todo> &todos)
+{
+}
+
+bool app::writeTSV(const std::vector<Todo> &todos)
+{
+  return false;
+}
+
+bool app::readTSV(const std::vector<Todo> &todos)
+{
+  return false;
+}
+
+void app::print(const std::vector<Todo> &todos)
+{
+}
+
+bool app::exit(bool isChanged)
+{
+  return false;
+}
--- Basic/app/oneDimensionArray/todo/Todo.hpp
+++ Basic/app/oneDimensionArray/todo/Todo.hpp
@@ -37,4 +37,49 @@ namespace app
     }
   };
 
+  /// @brief TODOリストに新しいTODOを追加します。
+  /// @param todos TODOリスト。
+  /// @return 正常にTODOを追加した場合True、追加できなかった場合falseを返します。
+  bool add(std::vector<Todo> &todos);
+
+  /// @brief TODOリストからTODOを削除します。
+  /// @param todos TODOリスト。
+  /// @return 正常にTODOを削除した場合True、削除できなかった場合はFalseを返します。
+  bool remove(std::vector<Todo> &todos);
+
+  /// @brief TODOリストを更新する。
+  /// @param todos TODOリスト。
+  /// @return 正常にTODOを更新できた場合True、更新できなかった場合False。
+  bool update(std::vector<Todo> &todos);
+
+  /// @brief タイトル名からTODOを検索します。
+  /// @param todos TODOリスト
+  void findByTitle(const std::vector<Todo> &todos);
+
+  /// @brief 期限日からTODOを検索します。
+  /// @param todos TODOリスト
+  void findByDueDate(const std::vector<Todo> &todos);
+
+  /// @brief 完了済みかどうかTODOを検索します。
+  /// @param todos TODOリスト
+  void findByCompleted(const std::vector<Todo> &todos);
+
+  /// @brief TODOリストをファイルに書き込みます。
+  /// @param todos TODOリスト
+  /// @return 正常に書き込まれた場合True、ファイルが開けないもしくは書き込みが行えなかった場合はFalseを返します。
+  bool writeTSV(const std::vector<Todo> &todos);
+
+  /// @brief TODOリストをファイルを読み込みます。
+  /// @param todos TODOリスト
+  /// @return 正常に読み込めた場合True、ファイルが開けなかった場合Falseを返します。
+  bool readTSV(const std::vector<Todo> &todos);
+
+  /// @brief 登録されているTODOを出力します。
+  /// @param todos TODOリスト
+  void print(const std::vector<Todo> &todos);
+
+  /// @brief TODOアプリケーションのEXIT制御
+  /// @param isChanged TODOがファイルに書き込まれていない場合True、書き込まれている場合False
+  /// @return TODOアプリケーションを終了する場合True、終了しない場合はFalseを返す。
+  bool exit(bool isChanged);
 }
