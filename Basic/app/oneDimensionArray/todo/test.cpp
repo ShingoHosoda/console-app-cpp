6f70f9de25ec8623d5f6fdb3fc0c3389f5c936a4
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-08 14:49:00 +0900

    削除関数のテストコード作成

--- Basic/app/oneDimensionArray/todo/test.cpp
+++ Basic/app/oneDimensionArray/todo/test.cpp
@@ -33,8 +33,41 @@ void TestAdd()
   std::cout << "テスト成功" << std::endl;
 }
 
+void TestRemove()
+{
+  std::cout << "テスト開始" << std::endl;
+  std::vector<app::Todo> todos1{
+      {"ID1", "タイトル1", "2025-02-10", 0},
+      {"ID2", "タイトル2", "2025-02-15", 0},
+      {"ID3", "タイトル3", "2025-02-20", 0},
+      {"ID4", "タイトル4", "2025-02-25", 0},
+      {"ID5", "タイトル5", "2025-02-10", 0},
+      {"ID6", "タイトル6", "2025-02-10", 0},
+  };
+
+  app::remove(todos1);
+  app::remove(todos1);
+
+  std::vector<app::Todo> todos2{
+      {"ID1", "タイトル1", "2025-02-10", 0},
+      {"ID2", "タイトル2", "2025-02-15", 0},
+      {"ID3", "タイトル3", "2025-02-20", 0},
+      {"ID4", "タイトル4", "2025-02-25", 0},
+  };
+
+  auto index = 0;
+  for (const auto &todo : todos1)
+  {
+    assert(todo.title == todos2[index].title);
+    index += 1;
+  }
+
+  std::cout << "テスト成功" << std::endl;
+}
+
 int main()
 {
-  TestAdd();
+  // TestAdd();
+  TestRemove();
   return 0;
 }
