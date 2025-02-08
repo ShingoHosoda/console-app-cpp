2de7e98bf2c80d8e2a4d2b464ee4f007d669726c
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-08 13:29:57 +0900

    ID生成関数の位置を移動

--- Basic/app/oneDimensionArray/todo/Todo.cpp
+++ Basic/app/oneDimensionArray/todo/Todo.cpp
@@ -1,5 +1,21 @@
 #include "Todo.hpp"
 
+std::string app::generateID(const std::string &candidateCharacters, std::size_t length)
+{
+  assert(!candidateCharacters.empty());
+  std::random_device engine;
+  std::uniform_int_distribution<std::size_t> dist(0, candidateCharacters.size() - 1);
+  std::string password;
+  for (std::size_t i = 0; i < length; i++)
+  {
+    std::size_t randomIndex = dist(engine);
+    char randomCharacter = candidateCharacters[randomIndex];
+    password += randomCharacter;
+  }
+
+  return password;
+}
+
 bool app::add(std::vector<Todo> &todos)
 {
   return false;
@@ -44,20 +60,4 @@ void app::print(const std::vector<Todo> &todos)
 bool app::exit(bool isChanged)
 {
   return false;
-}
-
-std::string app::generateID(const std::string &candidateCharacters, std::size_t length)
-{
-  assert(!candidateCharacters.empty());
-  std::random_device engine;
-  std::uniform_int_distribution<std::size_t> dist(0, candidateCharacters.size() - 1);
-  std::string password;
-  for (std::size_t i = 0; i < length; i++)
-  {
-    std::size_t randomIndex = dist(engine);
-    char randomCharacter = candidateCharacters[randomIndex];
-    password += randomCharacter;
-  }
-
-  return password;
 }
