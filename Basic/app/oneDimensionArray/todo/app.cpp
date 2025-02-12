504191f07c8d7cd5b450f38b666b95dff6b4fed1
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-12 09:12:18 +0900

    検索関数の実装

--- Basic/app/oneDimensionArray/todo/app.cpp
+++ Basic/app/oneDimensionArray/todo/app.cpp
@@ -11,6 +11,11 @@ int main()
   {
     std::cout << "-------------------------------------------\n"
               << "add: " << "TODOを新規に追加します。" << "\n"
+              << "remove: " << "TODOを削除します。" << "\n"
+              << "update: " << "TODOを更新します。" << "\n"
+              << "findByTitle: " << "タイトル名からTODOを検索します。" << "\n"
+              << "findByDueDate: " << "期限日からTODOを検索します。" << "\n"
+              << "findByCompleted: " << "完了済みかどうかTODOを検索します。。" << "\n"
               << "print: " << "TODOアプリを表示します。" << "\n"
               << "exit: " << "TODOアプリを終了します。" << "\n"
               << "-------------------------------------------\n";
@@ -22,6 +27,35 @@ int main()
       {
         isChanged = true; // 変更あり
       }
+    }
+    else if (command == "remove")
+    {
+      if (app::remove(todos))
+      {
+        isChanged = true; // 変更あり
+      }
+    }
+    else if (command == "update")
+    {
+      if (app::update(todos))
+      {
+        isChanged = true; // 変更あり
+      }
+    }
+    else if (command == "findByTitle")
+    {
+      auto title = app::findByTitle(todos);
+      app::print(title);
+    }
+    else if (command == "findByDueDate")
+    {
+      auto dueDate = app::findByDueDate(todos);
+      app::print(dueDate);
+    }
+    else if (command == "findByCompleted")
+    {
+      auto completed = app::findByCompleted(todos);
+      app::print(completed);
     }
     else if (command == "print")
     {
