f96f7586c632e83a0b0b22bfa239ec74c652adcc
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-14 09:26:21 +0900

    演算子のオーバーロードの修正

--- Basic/app/todo/TodoLess.hpp
+++ Basic/app/todo/TodoLess.hpp
@@ -6,7 +6,7 @@ namespace app
 {
   struct TodoLess
   {
-    bool operator()(const Todo &todo1, const Todo &todo2) const noexcept
+    constexpr bool operator()(const Todo &todo1, const Todo &todo2) const noexcept
     {
       return std::tie(todo1.id, todo1.title, todo1.dueDate, todo1.completed) < std::tie(todo2.id, todo2.title, todo2.dueDate, todo2.completed);
     }
