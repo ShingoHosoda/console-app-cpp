b96ae1e4c80c20160392d418604f233d852d954e
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-18 11:08:32 +0900

    読み込み関数のコメントを修正

--- app/letter/read-write/readLetter.hpp
+++ app/letter/read-write/readLetter.hpp
@@ -5,10 +5,10 @@
 #include <sstream>
 namespace letter
 {
-  /// @brief 手紙の本文のみ読み込む。
+  /// @brief 手紙の本文のみ読み込みます。
   /// @param path ファイルパス。
   /// @param letter 手紙本文のデータ。
-  /// @return 手紙本文のみ返す。
+  /// @return 読み込みが上手くいった場合true、そうでない場合はfalseを返します。。
   [[nodiscard]]
   bool readLetter(const std::string path, std::vector<std::string> &letter);
 }
