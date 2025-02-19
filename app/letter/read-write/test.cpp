582dafc931ea9b33015bcd901015e9d2c671b96b
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-19 09:15:36 +0900

    複数行の読み込みテストを追加

--- app/letter/read-write/test.cpp
+++ app/letter/read-write/test.cpp
@@ -18,9 +18,31 @@ void TestReadOneLineLetter()
   }
   std::cout << "テスト成功" << std::endl;
 }
+
+/// @brief テキストファイルの読み込みテスト
+void TestReadLetter()
+{
+  std::cout << "テスト開始" << std::endl;
+  std::vector<std::string> letter;
+  std::string inputPath{"data/input/private-text-letter.txt"};
+  assert(letter::readLetter(inputPath, letter) == true);
+  std::vector<std::string> oneLineLetter2{
+      {"PRIVATE advertisement no one knows (two today). Write me first"},
+      {"through lough. Disgrace you left behind cannot get over. Will"},
+      {"endeavour to stay where you left us till I hear from you, give"},
+      {"your advice. Your unhappy wife not well."}};
+  auto index = 0;
+  for (const auto &oneLineLetter1 : letter)
+  {
+    assert(oneLineLetter1 == oneLineLetter2[index]);
+    index += 1;
+  }
+  std::cout << "テスト成功" << std::endl;
+}
+
 int main()
 {
   TestReadOneLineLetter();
-
+  TestReadLetter();
   return 0;
 }
