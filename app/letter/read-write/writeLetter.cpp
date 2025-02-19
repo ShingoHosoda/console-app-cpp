a9895db56b16f7be2abd4d2dd90eab12f6e0bf42
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-19 09:42:22 +0900

    テキストファイルの書き込み関数の実装

---
+++ app/letter/read-write/writeLetter.cpp
@@ -0,0 +1,28 @@
+#include "writeLetter.hpp"
+
+bool letter::writeLetter(const std::string path, std::vector<std::string> &letter)
+{
+  std::ofstream ofs(path);
+  if (!ofs)
+  {
+    std::cout << "Not Found ファイルを書き込めませんでした。\n";
+    return false;
+  }
+
+  auto index = 0;
+  auto size = letter.size();
+  for (const auto &text : letter)
+  {
+    if (index == size - 1)
+    {
+      ofs << text; // 最終列のみ改行しない
+    }
+    else
+    {
+      ofs << text << "\n";
+    }
+    index += 1;
+  }
+
+  return true;
+}
---
+++ app/letter/read-write/writeLetter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+namespace letter
+{
+  /// @brief テキストファイルを書き込みます。
+  /// @param path ファイルパス。
+  /// @param letter テキストファイルのデータ。
+  /// @return 書き込み上手くいった場合true、そうでない場合はfalseを返します。
+  [[nodiscard]]
+  bool writeLetter(const std::string path, std::vector<std::string> &letter);
+}
